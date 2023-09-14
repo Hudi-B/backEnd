a9a047be7842964c0cb8b02396002a620cbf291c
Hudi-B <szegedi.bence.csaba@gmail.com>
2023-09-14 10:44:47 +0200

    added Post Pull Delete

--- product/productAPI/Controllers/ProductController.cs
+++ product/productAPI/Controllers/ProductController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Formatters;
+using System.Diagnostics.Eventing.Reader;
 using static productAPI.DTOs;
 
 namespace productAPI.Controllers
@@ -44,6 +45,41 @@ namespace productAPI.Controllers
             productDTOs.Add(product);
 
             return product;
+        }
+
+        [HttpPut]
+        public ProductDTO PullProduct(Guid id, UpdateProductDTO updateProduct)
+        {
+            var existingProduct = productDTOs.Where(x => x.Id == id).FirstOrDefault();
+
+            var product = existingProduct! with
+            {
+                ProductName = updateProduct.ProductName,
+                ProductPrice = updateProduct.ProductPrice,
+                ModifiedTime = DateTimeOffset.Now
+            };
+
+            var index = productDTOs.FindIndex(x => x.Id == id);
+
+            productDTOs[index] = product;
+
+            return product;
+        }
+
+        [HttpDelete]
+        public string DeleteProduct(Guid id)
+        {
+            var index = productDTOs.FindIndex(x => x.Id == id);
+
+            if (index != -1)
+            {
+                productDTOs.RemoveAt(index);
+                return "Sikeres torles";
+            }
+            else
+            {
+                return "Sikertelen torles";
+            }
         }
     }
 }
