3e63b2e8b1400875ba07df2c372e2db6f41b05d5
Hudi-B <szegedi.bence.csaba@gmail.com>
2023-09-14 08:55:46 +0200

    added product

---
+++ product/productAPI/Controllers/ProductController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using static productAPI.DTOs;
+
+namespace productAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        private static readonly List<ProductDTO> productDTOs = new()
+        {
+            new ProductDTO(Guid.NewGuid(), "Termék1", 3000, DateTimeOffset.Now, DateTimeOffset.Now),
+            new ProductDTO(Guid.NewGuid(), "Termék2", 3500, DateTimeOffset.Now, DateTimeOffset.Now),
+            new ProductDTO(Guid.NewGuid(), "Termék3", 2000, DateTimeOffset.Now, DateTimeOffset.Now)
+        };
+    }
+}
---
+++ product/productAPI/DTOs.cs
@@ -0,0 +1,8 @@
+﻿namespace productAPI
+{
+    public class DTOs
+    {
+        public record ProductDTO(Guid Id, string ProductName, int ProductPrice, DateTimeOffset CreatedTime, DateTimeOffset ModifiedTime);
+
+    }
+}
