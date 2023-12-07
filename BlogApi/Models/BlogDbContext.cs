dc2fb865875a5a6ea71955d9c079501f35859cac
Hudi-B <vanosskanoss@gmail.com>
2023-12-07 11:28:02 +0100

    lesson1207

---
+++ BlogApi/Models/BlogDbContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace BlogApi.Models
+{
+    public class BlogDbContext : DbContext
+    {
+        public BlogDbContext(DbContextOptions options) : base(options) 
+        {
+
+        }
+
+        public DbSet<BlogUser> BlogUsers { get; set; } = null!;
+        public DbSet<BlogUserContent> BlogUserContent { get; set; } = null!;
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                string conn = "server=192.168.50.169; database=Blog; user=root; password=password";
+
+                optionsBuilder.UseMySql(conn, ServerVersion.AutoDetect(conn));
+            }
+        }
+    }
+}
